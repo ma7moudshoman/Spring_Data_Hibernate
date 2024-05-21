85ad597bb137eaac400278d197a3de511c1013b5
ma7moudshoman <shomanm53@gmail.com>
2024-05-22 00:54:46 +0300

    SessionFactory and Session init

--- src/Com/SpringData/Hibernate/MainApp.java
+++ src/Com/SpringData/Hibernate/MainApp.java
@@ -1,13 +1,24 @@
 package Com.SpringData.Hibernate;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import Com.SpringData.Hibernate.model.Client;
 
 public class MainApp {
 public static void main(String[]args)  {
 	
-	String Url="jdbc:mysql://localhost:3306/employee?useSSL=false";
+	SessionFactory factory=new Configuration()
+			.configure("hibernate.cfg.xml")
+			.addAnnotatedClass(Client.class)
+			.buildSessionFactory();
+	 
+    Session session=factory.getCurrentSession();
+
+	
+	
+	/*String Url="jdbc:mysql://localhost:3306/employee?useSSL=false";
 	String username="root";
 	String Password="123456789"; 
 	
@@ -15,12 +26,12 @@ public static void main(String[]args)  {
 		Connection connection = DriverManager.getConnection(
 				Url , username , Password
 				);
-		System.out.println("connection !");
+		System.out.println("connection ");
 		System.out.println(connection);
 
 	} catch (SQLException e) {
 		System.out.println(e.toString());
 	}
-	
+	*/
 }
 }
--- src/Com/SpringData/Hibernate/model/Client.java
+++ src/Com/SpringData/Hibernate/model/Client.java
@@ -1,5 +1,51 @@
 package Com.SpringData.Hibernate.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "client")
 public class Client {
 
+	@Id
+	@Column(name = "Id")
+	private Long id;
+	@Column(name="client_Name")
+	private String fullName;
+	@Column(name = "client_Age")
+	private int age;
+	@Column(name = "address ")
+	private String address;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+	
+	
 }
