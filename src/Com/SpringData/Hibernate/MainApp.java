850b5e9efd16771106a17ed05a1906c44f6adb1b
ma7moudshoman <shomanm53@gmail.com>
2024-05-27 21:16:21 +0300

    client init

--- src/Com/SpringData/Hibernate/MainApp.java
+++ src/Com/SpringData/Hibernate/MainApp.java
@@ -13,16 +13,61 @@ public static void main(String[]args)  {
 			.configure("hibernate.cfg.xml")
 			.addAnnotatedClass(Client.class)
 			.buildSessionFactory();
-	 
+	
+	
+	
     Session session=factory.getCurrentSession();
 
-    long id=4;
+   long id=7;
     try {
 		session.beginTransaction();
-	Client client=	session.get(Client.class, id);
-	
-	
+	/*Client clientsession.get(Client.class, id);
+	client.setFullName("ahmed");
+	client.setAge(55);
+	client.setAddress("october");*/
+		
+		
+		
+		//       Update 
+		/*Client client=new Client("soha", 25,"el shohada");
+		client.setId((long) 4);
+
+		session.update(client);
+		
 		session.getTransaction().commit();
+		
+		*/
+		      //  delete   
+		        
+
+		  Client client=new Client();
+		client.setId(id);
+
+		session.delete(client);
+		
+		session.getTransaction().commit();
+		
+
+		/*
+		Client client =new Client();
+		Client client1=new Client("MAhmoud", 26, "Alix");
+		   client1.setId((long) 1);
+		   
+
+		   Client client2=new Client("Mohand", 18, "Cairo");
+		  client2.setId((long) 2);
+
+		  Client client3=new Client("Masen", 5, "here");
+		 client3.setId((long) 3);
+		  Client client4=new Client("sara", 62, "there");
+			 client4.setId((long) 4);
+
+			session.save(client1);
+			session.save(client2);
+			session.save(client3);
+			session.save(client4);
+			session.beginTransaction().commit();*/
+
 	System.out.println(client.getFullName() + " " + client.getAddress());
     } catch (Exception e) {
 		System.out.println(e.toString());
@@ -33,6 +78,8 @@ public static void main(String[]args)  {
     
 }
 /*
+                select
+              
  * Client client1=new Client("MAhmoud", 26, "Alix");
   // client1.setId((long) 1);
    
